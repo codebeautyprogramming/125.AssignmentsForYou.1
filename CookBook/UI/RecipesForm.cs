d8805322898a8337d487e30ce484dd2d1d65e432
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-03-30 14:49:57 +0100

    Startup code for this chapter

---
+++ CookBook/UI/RecipesForm.cs
@@ -0,0 +1,139 @@
+﻿using CookBook.Helpers;
+using DataAccessLayer.Contracts;
+using DomainModel.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookBook.UI
+{
+    public partial class RecipesForm : Form
+    {
+        private readonly IRecipeTypesRepository _recipeTypesRepository;
+        private readonly IRecipesRepository _recipesRepository;
+        private readonly IServiceProvider _serviceProvider;
+
+        private Image _placeholderImage
+        {
+            get
+            {
+                var executingAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var imagePath = Path.Combine(executingAssemblyLocation, "Assets\\Images\\recipe_placeholder_image.png");
+                return Image.FromFile(imagePath);
+            }
+        }
+        private bool _isUserImageAdded=false;
+
+
+        public RecipesForm(IRecipeTypesRepository recipeTypesRepository, IServiceProvider serviceProvider, IRecipesRepository recipesRepository)
+        {
+            InitializeComponent();
+            _recipeTypesRepository = recipeTypesRepository;
+            _recipesRepository = recipesRepository;
+            _serviceProvider = serviceProvider;
+            _recipesRepository.OnError += message => MessageBox.Show(message);
+        }
+
+        private async void RefreshRecipeTypes()
+        {
+            RecipeTypesCbx.DataSource = await _recipeTypesRepository.GetRecipeTypes();
+            RecipeTypesCbx.DisplayMember = "Name";
+        }
+        private async void RefreshRecipesGrid()
+        {
+            RecipesGrid.DataSource = await _recipesRepository.GetRecipes();
+        }
+
+        private void RecipesForm_Load(object sender, EventArgs e)
+        {
+            RefreshRecipeTypes();
+            RefreshRecipesGrid();
+            RecipePictureBox.Image = _placeholderImage;
+            RecipePictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void AddRecipeTypeBtn_Click(object sender, EventArgs e)
+        {
+            RecipeTypesForm form = _serviceProvider.GetRequiredService<RecipeTypesForm>();
+            form.FormClosed += (sender, e) => RefreshRecipeTypes();
+            form.ShowDialog();
+        }
+
+        private async void AddRecipeBtn_Click(object sender, EventArgs e)
+        {
+            if (!IsValid())
+                return;
+
+            byte[] image = null;
+            if(_isUserImageAdded)
+                image = ImageHelper.ConvertToDbImage(RecipePictureBox.ImageLocation);
+            int recipeTypeId = ((RecipeType)RecipeTypesCbx.SelectedItem).Id;
+            Recipe newRecipe = new Recipe(NameTxt.Text, DescriptionTxt.Text, image, recipeTypeId);
+
+            await _recipesRepository.AddRecipe(newRecipe);
+            ClearAllFields();
+            RefreshRecipesGrid();
+        }
+
+        private void ClearAllFields()
+        {
+            NameTxt.Text = string.Empty;
+            DescriptionTxt.Text = string.Empty;
+            RecipePictureBox.ImageLocation = string.Empty;
+            RecipePictureBox.Image = _placeholderImage;
+            _isUserImageAdded = false;
+        }
+
+        private bool IsValid()
+        {
+            bool isValid = true;
+            string message = "";
+
+            if (string.IsNullOrEmpty(NameTxt.Text))
+            {
+                isValid = false;
+                message += "Please enter name.\n\n";
+            }
+            if (string.IsNullOrEmpty(DescriptionTxt.Text))
+            {
+                isValid = false;
+                message += "Please enter description.\n\n";
+            }
+
+            if (!isValid)
+                MessageBox.Show(message, "Form not valid!");
+
+            return isValid;
+        }
+
+        private void RecipePictureBox_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Title = "Please select an image",
+                Filter = "PNG|*.png|JPG|*.jpg|JPEG|*.jpeg",
+                Multiselect = false
+            })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    RecipePictureBox.ImageLocation = openFileDialog.FileName;
+                    _isUserImageAdded = true;
+                }
+            }
+        }
+
+        private void ClearAllFieldsBtn_Click(object sender, EventArgs e)
+        {
+            ClearAllFields();
+        }
+    }
+}
